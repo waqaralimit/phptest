a28cc00673e75c57e28d4998163b6281430f3c34
Waqar Ali Shah <waqaralimit@yahoo.com>
2020-07-19 23:58:21 -0700

    Multiples of three print Fizz
    For multiples of three print Fizz instead of the number

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,17 +1,25 @@
 <?php 
 class FizzBuzz{
 	var $n;
-	function __construct($n)
+	var $atvalue1;
+	function __construct($n,$atvalue1)
 	{
 		$this->n=$n;
+		$this->atvalue1=$atvalue1;
 	}
-//Write a program called FizzBuzz that prints one line for each number from 1 to 20
-	function printnumbers(){
-			for($i=1; $i<=$this->n; $i++)
-				echo $i."</br>";
+	//For multiples of three print Fizz instead of the number
+	function printatmultiples(){
+			for($i=1; $i<=$this->n; $i++){
+				if($i%$this->atvalue1==0)
+					echo "Fizz</br>";
+				else
+					echo $i."</br>";
+					
+			}
+				
 	}
 	
 }
 
-$obj=new FizzBuzz(20);
-$obj->printnumbers();
+$obj=new FizzBuzz(20,3);
+$obj->printatmultiples();
